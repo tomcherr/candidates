ef5786be4a189ccd991915ba4f8097a9632d977e
tomcherr <vajitha232@gmail.com>
2023-08-17 11:03:33 +0530

    <commit-message>

--- candidates/candidateapp/forms.py
+++ candidates/candidateapp/forms.py
@@ -1,11 +1,7 @@
 # from django import forms  
 # from .models import Candidatedirectorys
 
-# class CandidateForm(forms.ModelForm):
-#     class Meta:
-#         model = Candidatedirectorys
-#         fields = '__all__'  
-#         exclude = ['status', 'created_date', 'created_by', 'modified_date', 'modified_by']
+
 from django import forms  
 from candidateapp.models import Candidatedirectory
 from django.forms import fields
